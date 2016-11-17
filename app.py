e70a3a14f919d21fdc967afd6fede4212e2190bb
kilpopas <pasi.kilponen@tieto.com>
2016-11-17 20:21:25 +0200

    added instructions to fire up build using curl

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from bottle import route, run
 
 @route('/')
 def index():
-    return "<h1> hello brave OpenShift Ninja Warrior</h1>"
+    return "<h1> hello brave OpenShift Ninja Warrior</h1><br>to fire up build do curl -X POST -v insert_generic_webhook_url_here</br>"
 
 if __name__ == '__main__':
     run(host='0.0.0.0', port=8080)
